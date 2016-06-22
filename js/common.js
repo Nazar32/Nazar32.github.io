4777c34a4bf4784ea5d47c5b9e6c4ba66833ca54
nazar3223@gmail.com <16051997n>
2016-06-22 13:36:42 +0300

    scripts

--- js/common.js
+++ js/common.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+	
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline", 
+		midClick: true
+	});
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".fade_left").animated("fadeInLeft", "fadeOutLeft");
+	$(".fade_right, .fade_right ul li a").animated("fadeInRight", "fadeOutRight");
+	$(".top_text p, .section_header, .fade_down").animated("fadeInUp", "fadeOutDown");
+	$(".fade_down").animated("rotateInUpLeft", "rotateOutUpLeft");
+
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutLeft");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutRight");
+	$(".resume_icon").animated("bounceInDown", "bounceOutDown");
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+
 	function heightDetect() {
 		$(".main_head").css("height", $(window).height());
 	}
@@ -12,19 +37,30 @@ $(document).ready(function() {
   		$(".sandwich").toggleClass("active");
 	});
 
+	$(".top_menu li").click(function() {
+		$(".top_menu").fadeOut(600);
+		$(".sandwich").removeClass("active");
+		$(".top_text").removeClass("h_opacify");
+	}).append("<span>");
+
 	
 	//animate menu opening and closing 
 	$(".toggle_menu").click(function() {
 		
 		if ($(".top_menu").is(":visible")) {
-			$(".top_menu").fadeOut(600);
+			$(".top_text").removeClass("h_opacify");
 			$(".top_menu li a").removeClass("fadeInUp animated");
+			$(".top_menu li a").addClass("fadeOutUp animated");
+			$(".top_menu").fadeOut(1500);
 		
 		}
 		else {
-			$(".top_menu").fadeIn(600);	
+			$(".top_menu li a").removeClass("fadeOutUp animated");
 			$(".top_menu li a").addClass("fadeInUp animated");
 			
+			$(".top_text").addClass("h_opacify");
+			$(".top_menu").fadeIn(600);	
+			 
 		}
 	});
 
@@ -37,6 +73,7 @@ $(document).ready(function() {
 			$(this).removeClass("pulse animated");
 	});
 
+	//animate logo while mouse is over
 	$(".logo_container img").hover(
 		function() {
 			$(this).addClass("rubberBand animated");
@@ -45,6 +82,11 @@ $(document).ready(function() {
 			$(this).removeClass("rubberBand animated");
 	});
 	
+	$(".portfolio_item").each(function(i) {
+		$(this).find(".popup_descr a").attr("href", "#work_" + i);
+		$(this).find(".port_descr").attr("id", "work_" + i)
+	});
+
 });
 
 
